f40d351f2e88cd568a3eab27b06cbb74ea909f91
bay <68962000+bay-puz@users.noreply.github.com>
2021-08-21 18:23:36 +0900

    サイズ変更

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,16 +1,31 @@
-const row = 3;
-const column = 5;
+var row = 0;
+var column = 0;
+
+document.getElementById("table").addEventListener("click", clickTable);
+document.getElementById("table").oncontextmenu = function() {rightClickTable(event); return false};
+document.getElementById("setSize").addEventListener("click", setSize);
 
 function setProblem() {
+    var tableElement = document.getElementById("table");
+    if (tableElement.innerText.length > 0) {
+        tableElement.innerText = "";
+    }
     var theadElement = getTableHeaderHtml(column);
     var tbodyElement = document.createElement("tbody");
     for (let index = 1; index <= column; index++) {
         tbodyElement.append(getRowHtml(index));
     }
-    var tableElement = document.getElementById("table");
     tableElement.append(theadElement, tbodyElement);
 };
-setProblem();
 
-document.getElementById("table").addEventListener("click", clickTable);
-document.getElementById("table").oncontextmenu = function() {rightClickTable(event); return false};
+function setSize() {
+    var rowNew = parseInt(document.getElementById("setRow").value);
+    var columnNew = parseInt(document.getElementById("setColumn").value);
+    if ( rowNew !== row || columnNew !== column ) {
+        row = rowNew;
+        column = columnNew;
+        setProblem();
+    }
+}
+
+setSize();
